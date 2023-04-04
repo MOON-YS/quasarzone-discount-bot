7b89eed95466007f2157595c8c4fc9397aa85b2a
choryang <mys990422@gmail.com>
2023-04-04 20:42:16 +0900

    init

--- discordbot.py
+++ discordbot.py
@@ -65,6 +65,41 @@ async def init(ctx):
     else : 
         print(init_response.status_code)
     
+@tasks.loop(seconds=300)
+async def every_5min():
+    global channel, previous_top, url, dUrl, marker
+    init_response = requests.get(url)
+    if init_response.status_code == 200:
+        html = init_response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        imgs = soup.select('.maxImg')
+        info_price = soup.select('.text-orange')
+        info_category = soup.select('.category')
+        info_title = soup.select('.ellipsis-with-reply-cnt')
+        info_link = soup.select('.subject-link')
+
+        current_top = str(info_link[2]['href'])
+        if previous_top != current_top:
+            for k in range(2, len(info_link)):
+                if info_link[k]['href'] == previous_top:
+                    marker = k-2
+                    break
+        
+            for i in range(0, marker):
+                s = [""]
+                s.append(info_category[i].contents[0])
+                s.append(info_price[i].contents[0])
+                s.append(dUrl + info_link[i+2]['href'])
+                d = ''+'\n'.join(s)+''
+                embed = discord.Embed(title = info_title[i].contents[0], description=d)
+                embed.set_thumbnail(url = imgs[i]["src"])
+                await channel.send(embed=embed)
+            
+            previous_top = current_top
+        else :
+            print("nothing to update")
+    else : 
+        print(init_response.status_code)
 
 @bot.command()
 async def test(ctx):
