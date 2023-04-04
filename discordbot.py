69f90fd7a059150744329034b08550f89bc326cc
choryang <mys990422@gmail.com>
2023-04-04 19:50:06 +0900

    init

--- discordbot.py
+++ discordbot.py
@@ -3,30 +3,72 @@ from distutils.sysconfig import PREFIX
 import discord
 from dotenv import load_dotenv
 import os
+import time
+from bs4 import BeautifulSoup
+import requests
+from discord.ext import tasks
+
 load_dotenv()
 
 PREFIX = os.environ['PREFIX']
 TOKEN = os.environ['TOKEN']
 
-client = discord.Client()
+bot = discord.Client()
 
-@client.event
+marker = 30
+url = "https://quasarzone.com/bbs/qb_saleinfo?page=1"
+dUrl = 'https://quasarzone.com'
+
+previous_top = ''
+channel = ''
+
+@bot.event
 async def on_ready():
     print(f'Logged in as {client.user}.')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.command()
+async def init(ctx):
+    global channel
+    channel = ctx.channel
+    init_response = requests.get(url)
+    if init_response.status_code == 200:
+        html = init_response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        imgs = soup.select('.maxImg')
+        info_price = soup.select('.text-orange')
+        info_category = soup.select('.category')
+        info_title = soup.select('.ellipsis-with-reply-cnt')
+        info_link = soup.select('.subject-link')
 
-    if message.content == f'{PREFIX}call':
-        await message.channel.send("callback!")
-
-    if message.content.startswith(f'{PREFIX}hello'):
-        await message.channel.send('Hello!')
-
+        current_top = str(info_link[2]['href'])
+        
+        if previous_top != current_top:
+            for k in range(2, len(info_link)):
+                if info_link[k]['href'] == previous_top:
+                    marker = k-2
+                    break
+        
+            for i in range(0, marker):
+                s = [""]
+                s.append(info_category[i].contents[0])
+                s.append('\n')
+                s.append(info_price[i].contents[0])
+                s.append('\n')
+                s.append(dUrl + info_link[i+2]['href'])
+                embed = discord.Embed(title = info_title[i].contents[0])
+                embed.set_image(imgs[i]["src"])
+                await channel.send(embed=embed)
+            
+            previous_top = current_top
+        else :
+            print("nothing to update")
+    else : 
+        print(init_response.status_code)
+    
+        
+    
 
 try:
-    client.run(TOKEN)
+    bot.run(TOKEN)
 except discord.errors.LoginFailure as e:
     print("Improper token has been passed.")
